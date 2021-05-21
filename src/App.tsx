d609d176d3ed7486035e6f0ed28b96f619cd8eeb
Atishay Jain <hritikjain707@gmail.com>
2021-05-21 11:32:26 +0530

    Init Commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Hello Type</h1>
+    </div>
+  );
 }
 
 export default App;
